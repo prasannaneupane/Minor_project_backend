f950a89943f4fca03d64d89ca6b306de90059e7b
Prasanna Raj Neupane <128725370+prasannaneupane@users.noreply.github.com>
2026-03-15 21:04:21 +0545

    Updated CORS settings

--- Backend_Minor_Project/resnet50_backend/api.py
+++ Backend_Minor_Project/resnet50_backend/api.py
@@ -35,7 +35,7 @@ app = FastAPI(
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3001", "http://localhost:5000", "*"],
+    allow_origins=["http://localhost:3001", "http://localhost:5000", "http://localhost:3000", "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
